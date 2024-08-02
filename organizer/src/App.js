90078aedbd62d57550e442a0ab746f0d6d0a9c2d
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-08-02 20:07:33 +0300

    adding Navigation.js

--- organizer/src/App.js
+++ organizer/src/App.js
@@ -1,10 +1,11 @@
 import { Router } from 'react-router-dom';
+import { Navigation } from './Navigation/Navigation';
 
 function App() {
   return (
     <div className="App">
         <Router>
-            
+            <Navigation />
         </Router>
     </div>
   );
---
+++ organizer/src/Navigation/Navigation.js
@@ -0,0 +1,15 @@
+
+
+export const Navigation = () => {
+    return (
+        <div className="navbar">
+            <ul className="navbat-ul">
+                <li>Home</li>
+                <li>Login</li>
+                <li>Register</li>
+                <li>Logout</li>
+                <li>Tasks</li>
+            </ul>
+        </div>
+    )
+}
