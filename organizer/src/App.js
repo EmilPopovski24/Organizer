9f47572119cdf2b6ad9256ab4ed71a8b3a0a7085
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-08-02 20:15:35 +0300

    Update App.js

--- organizer/src/App.js
+++ organizer/src/App.js
@@ -1,10 +1,15 @@
-
+import { Router, Routes } from 'react-router-dom';
 import { Navigation } from './Navigation/Navigation';
 
 function App() {
   return (
     <div className="App">
         <Navigation />
+          <div className='main-content'>
+              <Routes>
+                  <Route path ="/home" element={<Home />} />
+              </Routes>
+          </div>
     </div>
   );
 }
