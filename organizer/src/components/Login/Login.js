7c5e17534c8b7c5910f06b7b4992273f74fc72f6
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-08-03 15:29:35 +0300

    setup of Login and Register forms

--- organizer/src/components/Login/Login.js
+++ organizer/src/components/Login/Login.js
@@ -1,7 +1,16 @@
-
+import "./Login.css";
 
 export const Login = () => {
     return (
-        <p>Login page</p>
+        <>
+        <div class="login">
+	        <h1>Login</h1>
+            <form method="post">
+    	        <input type="text" name="u" placeholder="Username" required="required" />
+                <input type="password" name="p" placeholder="Password" required="required" />
+                <button type="submit" class="btn btn-primary btn-block btn-large">Login</button>
+            </form>
+        </div>
+        </>
     )
 }
--- organizer/src/components/Register/Register.js
+++ organizer/src/components/Register/Register.js
@@ -1,7 +1,17 @@
-
+import "./Register.css";
 
 export const Register = () => {
     return (
-        <p>Register page</p>
+        <>
+        <div class="register">
+	        <h1>Register</h1>
+            <form method="post">
+    	        <input type="text" name="username" placeholder="Username" required="required" />
+                <input type="password" name="password" placeholder="Password" required="required" />
+                <input type="password" name="confirm-password" placeholder="Confirm Password" required="required" />
+                <button type="submit" class="btn btn-primary btn-block btn-large">Register</button>
+            </form>
+        </div>
+        </>
     )
 }
