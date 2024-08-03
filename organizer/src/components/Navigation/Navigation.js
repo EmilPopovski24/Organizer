5fb9a76f0478b5a89b7f2bee560c0c9d65343812
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-08-03 14:47:51 +0300

    adding links to Navbar

--- organizer/src/components/Navigation/Navigation.js
+++ organizer/src/components/Navigation/Navigation.js
@@ -1,15 +1,16 @@
 import "./Navigation.css";
+import { Link } from "react-router-dom";
 
 export const Navigation = () => {
 
     return (
         <div className="navbar">
             <ul className="navbar-ul">
-                <li className="navbar-li">Home</li>
-                <li className="navbar-li">Login</li>
-                <li className="navbar-li">Register</li>
-                <li className="navbar-li">Logout</li>
-                <li className="navbar-li">My Tasks</li>
+                <li className="navbar-li"><Link to="/">Home</Link></li>
+                <li className="navbar-li"><Link to="/login">Login</Link></li>
+                <li className="navbar-li"><Link to="/register">Register</Link></li>
+                <li className="navbar-li"><Link to="/logout">Logout</Link></li>
+                <li className="navbar-li"><Link to="/tasks">My Tasks</Link></li>
             </ul>
         </div>
     )
