d5079ed4d07490216a8544f72038ca4a2f902cd2
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-08-03 17:17:40 +0300

    adding new Home page

--- organizer/src/App.js
+++ organizer/src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import { Navigation } from './components/Navigation/Navigation';
+import { Home } from './components/Home/Home';
 import { Login } from './components/Login/Login';
 import { Register } from './components/Register/Register';
 import { Logout } from './components/Logout/Logout';
