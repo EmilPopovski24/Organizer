2c4e37d9b76acf94c63f8c371a7162a668c94021
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-08-04 16:03:26 +0300

    NewTask update

--- organizer/src/components/NewTask/NewTask.js
+++ organizer/src/components/NewTask/NewTask.js
@@ -1,4 +1,4 @@
-
+import "./NewTask.css";
 
 export const NewTask = () => {
 
@@ -6,19 +6,20 @@ export const NewTask = () => {
 
     return (
         <>
-        <h1 className='addNewTask'>Add New Task</h1>
+        
             <section id ="create-page">
-                <form id="create" method="POST">
+                <form className="create" method="POST">
+                    <h1 className='addNewTask'>Add New Task</h1>
                     <label htmlFor="task-name">Task Name</label>
-                    <input type="text" id="taskName" name="taskName" placeholder="Task name..." className='parameters'/>
-                    <hr />
+                    <input type="text" id="taskName" name="taskName" placeholder="Task name..." className='parameters' required/>
+
                     <label htmlFor="description">Description:</label>
-                    <textarea className="description" name="task-description" id="task-description" required></textarea>
-                    <hr />
+                    <textarea name="task-description" id="task-description" className='parameters' required></textarea>
+
                     <label htmlFor="date">Date</label>
-                    <input type="datetime-local" id="task-datedate" name="task-date" className='parameters'/>
-                    <hr />
-                    <button type="submit" className="addTaskBtn">Add</button>
+                    <input type="datetime-local" id="task-datedate" name="task-date" className='parameters' required/>
+
+                    <button type="submit" className="addTaskBtn">Add Task</button>
                 </form>
             </section>
         </>
