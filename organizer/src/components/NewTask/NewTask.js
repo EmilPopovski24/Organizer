2f9b4482269a46715ed473d40c50dffb5aab9df0
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-08-04 15:38:56 +0300

    Update NewTask.js

--- organizer/src/components/NewTask/NewTask.js
+++ organizer/src/components/NewTask/NewTask.js
@@ -9,13 +9,16 @@ export const NewTask = () => {
         <h1 className='addNewTask'>Add New Task</h1>
             <section id ="create-page">
                 <form id="create" method="POST">
-                    <label htmlFor="task-title">Task Name</label>
-                    <input type="text" id="name" name="taskName" placeholder="Task name..." className='parameters'/>
+                    <label htmlFor="task-name">Task Name</label>
+                    <input type="text" id="taskName" name="taskName" placeholder="Task name..." className='parameters'/>
                     <hr />
-                    <label htmlFor="task-title">Description</label>
-                    <input type="textarea" id="description" name="description" placeholder="Description..." className='parameters'/>
+                    <label htmlFor="description">Description:</label>
+                    <textarea className="description" name="task-description" id="task-description" required></textarea>
                     <hr />
-                    <button type="submit" className="addPetBtn">Add</button>
+                    <label htmlFor="date">Date</label>
+                    <input type="datetime-local" id="task-datedate" name="task-date" className='parameters'/>
+                    <hr />
+                    <button type="submit" className="addTaskBtn">Add</button>
                 </form>
             </section>
         </>
