6c045e9b9fb3266edb568dc036c695ffe6bddd07
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-08-04 16:11:04 +0300

    Update Footer.js

--- organizer/src/components/Footer/Footer.js
+++ organizer/src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 export const Footer = () => {
     return (
         <div class="footer">
-            <p>Footer</p>
+            <h4 className="footer">All rights reserved &#169; 2024</h4>
         </div>
     )
 }
