ed8ac20716ba58303deae1038043bf5770cad56e
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-08-11 13:07:50 +0300

    update requester

--- organizer/src/services/taskService.js
+++ organizer/src/services/taskService.js
@@ -1,13 +1,18 @@
-import { request } from "./requester";
+import * as request from "./requester";
 
 const baseUrl = "http://localhost:3030/data/tasks"
 
-export const taskServiceFactory = (token) => {
-    
+// export const taskServiceFactory = (token) => {
+
+    export const getAll = async() => {
+        const result = await request.get(baseUrl)
+        const tasks = Object.values(result);
+        return tasks
+    }
  
 
-return {
+// return {
 
-}
+// }
 
-}
+// }
