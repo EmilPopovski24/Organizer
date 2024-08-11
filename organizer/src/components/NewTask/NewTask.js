b6e3eecc15c874584df4faf39811391c3ba1bbb0
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-08-11 14:41:12 +0300

    update

--- organizer/src/components/NewTask/NewTask.js
+++ organizer/src/components/NewTask/NewTask.js
@@ -1,20 +1,41 @@
+import { useState } from "react";
+
 import "./NewTask.css";
 
-export const NewTask = () => {
+export const NewTask = ({
+    onNewTaskSubmit
+}) => {
+
+    const [values, setValues] = useState({
+        taskName: '',
+        taskDescription: '',
+        taskDate: '',
+    })
+
+    const onChangeHandler = (e) => {
+        setValues(state => ({...state, [e.target.name]: e.target.value} ))
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        onNewTaskSubmit(values);
+    }
 
     return (
         <>
             <section id ="create-page">
-                <form className="create" method="POST">
+                <form className="create" method="POST" onSubmit={onSubmit}>
                     <h1 className='addNewTask'>Add New Task</h1>
+
                     <label htmlFor="task-name">Task Name</label>
-                    <input type="text" id="taskName" name="taskName" placeholder="Task name..." className='parameters' required/>
+                    <input type="text" id="taskName" name="taskName" value ={values.taskName} onChange={onChangeHandler} placeholder="Task name..." className='parameters' required/>
 
                     <label htmlFor="description">Description:</label>
-                    <textarea name="task-description" id="task-description" className='parameters' required></textarea>
+                    <textarea  id="taskDescription" name="taskDescription" value ={values.taskDescription} onChange={onChangeHandler} className='parameters' required></textarea>
 
                     <label htmlFor="date">Date</label>
-                    <input type="datetime-local" id="task-datedate" name="task-date" className='parameters' required/>
+                    <input type="datetime-local" id="taskDate" name="taskDate" value ={values.taskDate} onChange={onChangeHandler} className='parameters' required/>
 
                     <button type="submit" className="addTaskBtn">Add Task</button>
                 </form>
