f7453de3af2752ff250f7f5c5e285c799db89492
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-08-12 20:32:23 +0300

    Update NewTask.js

--- organizer/src/components/NewTask/NewTask.js
+++ organizer/src/components/NewTask/NewTask.js
@@ -30,8 +30,8 @@ export const NewTask = ({
                     <label htmlFor="task-name">Task Name</label>
                     <input type="text" id="taskName" name="taskName" value ={values.taskName} onChange={onChangeHandler} placeholder="Task name..." className='parameters' required/>
 
-                    <label htmlFor="description">Description:</label>
-                    <textarea  id="taskDescription" name="taskDescription" value ={values.taskDescription} onChange={onChangeHandler} className='parameters' required></textarea>
+                    <label htmlFor="description">Description</label>
+                    <textarea id="taskDescription" name="taskDescription" value ={values.taskDescription} onChange={onChangeHandler} className='parameters' required></textarea>
 
                     <label htmlFor="date">Date</label>
                     <input type="datetime-local" id="taskDate" name="taskDate" value ={values.taskDate} onChange={onChangeHandler} className='parameters' required/>
